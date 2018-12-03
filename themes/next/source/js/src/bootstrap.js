858e399640991316f12becc59463cafbe0226fd5
shamopoo <2570119235@qq.com>
2018-12-03 15:51:25 +0800

    :star: update blog

--- themes/next/source/js/src/bootstrap.js
+++ themes/next/source/js/src/bootstrap.js
@@ -13,17 +13,17 @@ $(document).ready(function () {
   NexT.utils.registerBackToTop();
 
   // Mobile top menu bar.
-  $('.site-nav-toggle button').on('click', function () {
-    var $siteNav = $('.site-nav');
-    var ON_CLASS_NAME = 'site-nav-on';
-    var isSiteNavOn = $siteNav.hasClass(ON_CLASS_NAME);
-    var animateAction = isSiteNavOn ? 'slideUp' : 'slideDown';
-    var animateCallback = isSiteNavOn ? 'removeClass' : 'addClass';
+  // $('.site-nav-toggle button').on('click', function () {
+  //   var $siteNav = $('.site-nav');
+  //   var ON_CLASS_NAME = 'site-nav-on';
+  //   var isSiteNavOn = $siteNav.hasClass(ON_CLASS_NAME);
+  //   var animateAction = isSiteNavOn ? 'slideUp' : 'slideDown';
+  //   var animateCallback = isSiteNavOn ? 'removeClass' : 'addClass';
 
-    $siteNav.stop()[animateAction]('fast', function () {
-      $siteNav[animateCallback](ON_CLASS_NAME);
-    });
-  });
+  //   $siteNav.stop()[animateAction]('fast', function () {
+  //     $siteNav[animateCallback](ON_CLASS_NAME);
+  //   });
+  // });
 
   /**
    * Register JS handlers by condition option.
